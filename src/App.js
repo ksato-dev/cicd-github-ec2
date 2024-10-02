0febdc40351784b0e89a248bf0067823168732eb
ksato-dev <sksoft.tech2020@gmail.com>
2024-10-02 17:47:19 +0900

    Updated again.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          WELCOME
+          WELCOME TO EASYDEPLOY
         </p>
         <a
           className="App-link"
